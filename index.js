388f0b56f9f6ed1b0cccc3fcbf038a23e8edab99
Anuj <anujverma3553@gmail.com>
2023-11-10 10:02:00 +0530

    PORT added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const connectToMongo = require('./db');
 connectToMongo();
 const app = express();
-const port = process.env.BASE_URL;
+const port = process.env.BASE_URL || 5000;
 app.use(cors());
 app.use(express.json());
 
